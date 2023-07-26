99d6a57351c7caacf2df4ae5422b71a32b30431e
Syed Muhammad Ali Shoukat <i202303@nu.edu.pk>
2023-07-26 16:22:51 +0500

    userSchema added

---
+++ backend/models/userModel/userSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        required: true
+    },
+    userId:{
+        type: Number,
+        required: true
+    }
+    }
+
+);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
