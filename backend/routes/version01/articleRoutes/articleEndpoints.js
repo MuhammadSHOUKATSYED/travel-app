4856af4a70ba6e9336f3a85cbc10ec36cbe4ea09
Syed Muhammad Ali Shoukat <i202303@nu.edu.pk>
2023-07-26 18:59:02 +0500

    articleEndpoints added

---
+++ backend/routes/version01/articleRoutes/articleEndpoints.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const articleRouter = express.Router;
+const articleController = require('../../../controllers/articleController');
+
+articleRouter.get('/', articleController.index);
+articleRouter.get('/:id', articleController.get);
+articleRouter.post('/', articleController.create);
+articleRouter.put('/:id', articleController.update);
+articleRouter.delete('/:id', articleController.delete);
+
+module.exports = articleRouter;
