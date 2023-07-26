b107b6f3213e60649feca3955a8071a24bd84928
Syed Muhammad Ali Shoukat <i202303@nu.edu.pk>
2023-07-26 22:06:48 +0500

    forumController added

---
+++ backend/controllers/forumController/forumController.js
@@ -0,0 +1,103 @@
+const BaseController = require('./index');
+const ForumModel = require('../../models/forumModel/forumSchema');
+
+class ForumController {
+
+
+static index =  async (req, res, next) =>{
+    try {
+        const forums = await ForumModel.find({});
+        res.json(
+            forums.map((forum) => ({
+                question: forum.question,
+                questionId: forum.questionId,
+                userId: forum.userId,
+                answer: forum.answer,
+            }))
+        );
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving forums');
+    }
+}
+static create  = async (req, res, next) =>{
+    const newForum = new ForumModel({
+        question: req.body.question,
+        questionId: req.body.questionId,
+        userId: req.body.userId,
+        answer: req.body.answer,
+    });
+
+    try {
+        const savedForum = await newForum.save();
+        res.status(201).json(savedForum);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error saving new Forum');
+    }
+}
+static update = async (req, res, next) =>{
+    try {
+
+
+        findRecord = await ForumModel.findOne({ questionId: req.body.questionId })
+        if(findRecord){
+            const updatedForum = await ForumModel.findByIdAndUpdate(
+                findRecord._id,
+                {
+                    question: req.body.question,
+                    questionId: req.body.questionId,
+                    userId: req.body.userId,
+                    answer: req.body.answer, 
+                },
+            );
+            res.json(updatedForum);    
+        }else {
+            res.send("No Such Record Found")
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error updating Forum');
+    }
+}
+static delete = async (req, res, next) =>{
+    try {
+        findRecord = await ForumModel.findOne({ questionId: req.params.questionId })
+        if(findRecord){
+            const removedRecord = await ForumModel.findByIdAndRemove(findRecord._id);
+            res.json(removedRecord);
+        }
+        else{
+            res.status(404).send("Forum Not Found")
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error deleting Forum');
+    }
+}
+static get = async (req, res, next) =>{
+    try {
+        findRecord = await ForumModel.findOne({ questionId: req.params.questionId })
+        if(findRecord){
+            const admin = await ForumModel.findById(findRecord._id);
+            res.json(
+                admin.map((forum) => ({
+                    question: forum.question,
+                    questionId: forum.questionId,
+                    userId: forum.userId,
+                    answer: forum.answer,
+                }))
+            );
+        }
+        else{
+            res.status(404).send("Forum Not Found")
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving Forum');
+    }
+}
+
+}
+
+modul.exports = ForumController;
--- backend/routes/version01/forumRoutes/forumEndpoints.js
+++ backend/routes/version01/forumRoutes/forumEndpoints.js
@@ -2,10 +2,10 @@ const express = require('express');
 const forumRouter = express.Router;
 const forumController = require('../../../controllers/forumController/forumController');
 
-forumRouter.get('/',forumController.index);
-forumRouter.get('/:id',forumController.get);
-forumRouter.post('/',forumController.create);
-forumRouter.put('/:id',forumController.update);
-forumRouter.delete('/:id',forumController.delete);
+forumRouter.get('/', forumController.index);
+forumRouter.get('/:id', forumController.get);
+forumRouter.post('/', forumController.create);
+forumRouter.put('/:id', forumController.update);
+forumRouter.delete('/:id', forumController.delete);
 
 module.exports = forumRouter;
