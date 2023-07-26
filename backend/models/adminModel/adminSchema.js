1a03605d63d8aba24b1972c3ec6bfe73a98535db
Syed Muhammad Ali Shoukat <i202303@nu.edu.pk>
2023-07-26 16:09:11 +0500

    adminSchema added

---
+++ backend/models/adminModel/adminSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema(
+    {
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        required: true
+    },
+    adminId:{
+        type: Number,
+        required: true
+    }
+    }
+
+);
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;
