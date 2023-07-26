a801d524d51aa00bcafb5099c43cacafaa775710
Syed Muhammad Ali Shoukat <i202303@nu.edu.pk>
2023-07-26 17:24:11 +0500

    superAdminShema added and errors removed

---
+++ backend/models/superAdminModel/superAdminSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const superAdminSchema = new mongoose.Schema(
+    {
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        required: true
+    },
+    superAdminId:{
+        type: Number,
+        required: true
+    }
+    }
+
+);
+const superAdmin = mongoose.model("SuperAdmin", superAdminSchema);
+
+module.exports = superAdmin;
