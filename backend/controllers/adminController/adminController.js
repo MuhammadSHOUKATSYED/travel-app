f50eb9f999a0c1a0462043fa4d64f85df550d84b
Syed Muhammad Ali Shoukat <i202303@nu.edu.pk>
2023-07-26 21:22:34 +0500

    adminController added

--- backend/controllers/adminController/adminController.js
+++ backend/controllers/adminController/adminController.js
@@ -1,15 +1,111 @@
-const getAdmins=async (req,res, next)=>{
+const BaseController = require('./index');
+const AdminModel = require('../../models/adminModel/adminSchema')
+
+class AdminController {
+
+
+static index =  async (req, res, next) =>{
+    try {
+        const admins = await AdminModel.find({});
+        res.json(
+            admins.map((admin) => ({
+                name: admin.name,
+                userName: admin.userName,
+                email: admin.email,
+                password: admin.password,
+                role: admin.role,
+                adminId: admin.adminId
+            }))
+        );
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving admins');
+    }
+}
+static create  = async (req, res, next) =>{
+    const newAdmin = new AdminModel({
+        name: req.body.name,
+        userName: req.body.userName,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role,
+        adminId: req.body.adminId,
+    });
+
+    try {
+        const savedAdmin = await newAdmin.save();
+        res.status(201).json(savedAdmin);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error saving new Admin');
+    }
+}
+static update = async (req, res, next) =>{
+    try {
+
+
+        findRecord = await AdminModel.findOne({ adminId: req.body.adminId })
+        if(findRecord){
+            const updatedAdmin = await AdminModel.findByIdAndUpdate(
+                findRecord._id,
+                {
+                    name: req.body.name,
+                    userName: req.body.userName,
+                    email: req.body.email,
+                    password: req.body.password,
+                    role: req.body.role,
+                    adminId: req.body.adminId,
+                },
+            );
+            res.json(updatedAdmin);    
+        }else {
+            res.send("No Such Record Found")
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error updating Admin');
+    }
+}
+static delete = async (req, res, next) =>{
+    try {
+        findRecord = await AdminModel.findOne({ adminId: req.params.adminId })
+        if(findRecord){
+            const removedRecord = await AdminModel.findByIdAndRemove(findRecord._id);
+            res.json(removedRecord);
+        }
+        else{
+            res.status(404).send("Admin Not Found")
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error deleting Admin');
+    }
+}
+static get = async (req, res, next) =>{
+    try {
+        findRecord = await AdminModel.findOne({ adminId: req.params.adminId })
+        if(findRecord){
+            const admin = await AdminModel.findById(findRecord._id);
+            res.json(
+                admin.map((admin) => ({
+                    name: admin.name,
+                    userName: admin.userName,
+                    email: admin.email,
+                    password: admin.password,
+                    role: admin.role,
+                    adminId: admin.adminId
+                }))
+            );
+        }
+        else{
+            res.status(404).send("Admin Not Found")
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving Admin');
+    }
+}
 
 }
-const getAdminById=async (req,res, next)=>{
 
-}
-const createAdmin=async (req, res, next)=>{
-
-}
-const updateAdmin=async (req,res, next)=>{
-
-}
-const deleteAdmin=async (req,res, next)=>{
-
-}
+modul.exports = AdminController;
--- backend/models/adminModel/adminSchema.js
+++ backend/models/adminModel/adminSchema.js
@@ -6,7 +6,7 @@ const adminSchema = new mongoose.Schema(
         type: String,
         required: true
     },
-    username: {
+    userName: {
         type: String,
         required: true
     },
