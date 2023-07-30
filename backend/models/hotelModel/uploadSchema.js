0f19cb012d9035b6378aadacef4a1e40a2a4379b
Syed Muhammad Ali Shoukat <i202303@nu.edu.pk>
2023-07-30 19:17:23 +0500

    Error fixed

--- backend/models/hotelModel/uploadSchema.js
+++ backend/models/hotelModel/uploadSchema.js
@@ -1,11 +1,7 @@
 // for images of the hotel
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-const UploadSchema = new mongoose.schema({
-=======
 const hotels=require('./hotelSchema')
 const UploadSchema = mongoose.schema({
->>>>>>> 43e2c7a259a5fd1c7cfd6cc14063000c0855be91
   fileName: {
     type: String,
     required: true,
@@ -23,10 +19,5 @@ const UploadSchema = mongoose.schema({
     ref:hotels.collection.name,
   }
 });
-<<<<<<< HEAD
-const Uploads = mongoose.model("Image", UploadSchema);
-module.exports = Uploads;
-=======
 const uploads=mongoose.model("Image", UploadSchema);
 module.exports = uploads
->>>>>>> 43e2c7a259a5fd1c7cfd6cc14063000c0855be91
